e0c007dce613d156b129defd05b6091dbe037942
Eshin_V <vladeshin@gmail.com>
2023-01-30 18:17:46 +0300

    vse verno pereimenova

---
+++ leccii/lec 2/Proba/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введи число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+double f(double x)
+{
+ double result = x * x + 1;
+ return result;
+}
+Console.WriteLine(result);
