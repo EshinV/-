58224429dde14541ae7f22387493592b7ecae556
Eshin_V <vladeshin@gmail.com>
2023-02-13 22:38:40 +0300

    Немного лек3 и сем4

---
+++ leccii/lec 3/1/Program.cs
@@ -0,0 +1,50 @@
+﻿//vid 1
+void Method1()
+{
+ Console.WriteLine("Author ...");
+}
+Method1();
+
+
+//Vid 2
+void Method2(string msg)
+{
+ Console.WriteLine(msg);
+}
+Method2("Text messege");
+
+void Method21(string msg, int count)
+{
+    int i=0;
+    while (i<count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21("Text messege",4); // lobo // Method21(msg:"Text messege", count:4) libo // Method21(count:4,msg:"Text messege") 
+
+//vid 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+//vid 4
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i<count)
+    {
+        result= result+text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4 (10, "qwe");
+Console.WriteLine(res);
---
+++ seminari/sem4/1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, котоорая принимает на вход число (А) и выдаёт сцмму чисел от 1 до А
+
+Console.WriteLine("Введите первое число А: ");
+int numA = int.Parse(Console.ReadLine());
+if (numA<1)
+{
+    Console.WriteLine("Число А меньше 1: ");
+}
+else
+{
+    int numsum = 0;
+    for (int i = 1; i <= numA; i++)
+    {
+   numsum += i;//numsum=numsum+i
+    }
+    Console.WriteLine($"сумма = {numsum}");
+}
+
