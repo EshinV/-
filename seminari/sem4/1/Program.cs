a1ce6098beacc6dc986d3e01c2fffb6f70b9b01f
Eshin_V <vladeshin@gmail.com>
2023-02-19 22:14:29 +0300

    Sem 4 i HW4

--- seminari/sem4/1/Program.cs
+++ seminari/sem4/1/Program.cs
@@ -1,18 +1,43 @@
 ﻿//Напишите программу, котоорая принимает на вход число (А) и выдаёт сцмму чисел от 1 до А
 
+// Console.WriteLine("Введите первое число А: ");
+// int numA = int.Parse(Console.ReadLine());
+
+// if (numA<1)
+// {
+//     Console.WriteLine("Число А меньше 1: ");
+// }
+// else
+// {                                                            //БЕЗ МЕТОДА
+//     int sum = 0;
+//     for (int i = 1; i <= numA; i++)
+//     {
+//         sum += i;//numsum=numsum+i
+//     }
+//     Console.WriteLine($"сумма = {sum}");
+// }
+
 Console.WriteLine("Введите первое число А: ");
 int numA = int.Parse(Console.ReadLine());
+
 if (numA<1)
 {
     Console.WriteLine("Число А меньше 1: ");
 }
 else
-{
-    int numsum = 0;
-    for (int i = 1; i <= numA; i++)
-    {
-   numsum += i;//numsum=numsum+i
-    }
-    Console.WriteLine($"сумма = {numsum}");
+{                                                          
+  int result = GetSum(numA);
+    Console.WriteLine($"сумма = {result}");
+    Console.WriteLine($"сумма = {GetSum(numA)}");
 }
 
+int GetSum (int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;//numsum=numsum+i
+    }
+    return sum;
+   
+} 
---
+++ seminari/sem4/3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Решение в группах задач:
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int numA = int.Parse(Console.ReadLine());
+
+if (numA<1)
+{
+    Console.WriteLine("Число А меньше 1: ");
+}
+else
+{                                                          
+  int result = GetSum(numA);
+    Console.WriteLine($"произведение = {result}");
+    Console.WriteLine($"произведение = {GetSum(numA)}");
+}
+
+int GetSum (int number)
+{
+    int com = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        com=i*com;//numsum=numsum+i
+    }
+    return com;
+   
+} 
