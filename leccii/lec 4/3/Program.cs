d72863513e7da280c59fb351242c16649fceaa37
Eshin_V <vladeshin@gmail.com>
2023-02-27 18:53:32 +0300

    lec 5 full

---
+++ leccii/lec 4/3/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//РЕКУРСИИ
+Console.WriteLine("Factorial");
+double Factorial(int n)
+{
+ // 1! = 1
+ // 0! = 1
+ if(n == 1) return 1;
+ else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
+Console.WriteLine("Fibonachi");
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f{i} = {Fibonacci(i)}");
+}
