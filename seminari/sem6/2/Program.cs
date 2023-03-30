47acbfa5c28c3db0701232a1b5e9e63c18164316
Eshin_V <vladeshin@gmail.com>
2023-03-30 21:09:59 +0300

    sem6 and HW

--- seminari/sem6/2/Program.cs
+++ seminari/sem6/2/Program.cs
@@ -2,31 +2,33 @@
 // Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
 // *Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.*
 
+// Console.WriteLine("Задача 40:");
+// int SetNumber(string numberName)
+// {
+//     Console.WriteLine($"Введите {numberName}:");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     return num;
+// }
 
-int SetNumber(string numberName)
-{
-    Console.WriteLine($"Введите {numberName}:");
-    int num = Convert.ToInt32(Console.ReadLine());
-    return num;
-}
 
-int a40 = SetNumber ("сторону А");
-int b40 = SetNumber ("сторону B");
-int c40 = SetNumber ("сторону C");
 
-bool IsTriangle(int a,int b,int c)  // - способ что предложил Габиль
-{
-return (((a+b)>c40)&&((a+c)>b)&&((b+c)>a));
-}
+// int a40 = SetNumber ("сторону А");
+// int b40 = SetNumber ("сторону B");
+// int c40 = SetNumber ("сторону C");
 
-if (IsTriangle(a40,b40,c40) )
-{
-    Console.WriteLine("треугольник");
-}
-else
-{
-    Console.WriteLine("не треугольник");
-}
+// bool IsTriangle(int a,int b,int c)  // - способ что предложил Габиль
+// {
+// return (((a+b)>c40)&&((a+c)>b)&&((b+c)>a));
+// }
+
+// if (IsTriangle(a40,b40,c40) )
+// {
+//     Console.WriteLine("треугольник");
+// }
+// else
+// {
+//     Console.WriteLine("не треугольник");
+// }
 
 // if ((a40 > (b40 + c40) ) || (b40 > (a40 + c40) )  || (c40 > (b40 + a40)))  - // мой способ
 // {
@@ -39,12 +41,31 @@ else
 
 
 
-остановился на 1:52:12 
-
-
 
 // Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
 // 45 -> 101101
 // 3 -> 11
 // 2-> 10
+
+Console.WriteLine("Задача 42:");
+Console.WriteLine("Введите число ");
+int a42 = Convert.ToInt32(Console.ReadLine());
+
+
+int [] Numbers42(int num42)
+{
+    int size42 = 32;
+    int [] result42 = new int [size42];
+    for (int i = 0; i < size42; i++)
+    {
+        result42[i] = num42 % 2;
+        num42/=2;
+    }
+    return result42;
+}
+
+int []  Bin24 = (Numbers42(a42));
+Array.Reverse(Bin24);
+
+Console.WriteLine("Полученный массив = " + String.Join(" ", Bin24));
---
+++ seminari/sem6/HW6/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+// System.Console.WriteLine("Задача 41.");
+// System.Console.WriteLine("Введите целые числа через пробел:");
+
+// int[] mas41 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+// int CountPositiveNumbers(int[] array)  
+// {
+//   int count = 0;
+//   for (int i = 0; i < array.Length; i++)
+//   {
+//     if (array[i] > 0)
+//       count++;
+//   }
+//   return count;
+// }
+
+// System.Console.WriteLine($"Пользователь ввел {CountPositiveNumbers(mas41)} чисел больше 0");
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.Write("Задача 43.");
+     
+System.Console.Write("Задайте значение b1: ");
+double b1 = int.Parse(Console.ReadLine());
+
+System.Console.Write("Задайте значение b2: ");
+double b2 = int.Parse(Console.ReadLine());
+
+System.Console.Write("Задайте значение k1: ");
+double k1 = int.Parse(Console.ReadLine());
+
+System.Console.Write("Задайте значение k2: ");
+double k2 = int.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
+
+if ((y != k1 * x + b1) || (k1 - k2 == 0)) System.Console.WriteLine("Решений нет!");
+  else
+  {
+    System.Console.WriteLine($"Координаты пересечения двух прямых, x, y: ({x}); ({y})");
+  }
